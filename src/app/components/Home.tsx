d84d8f804601eae016b6f51674f3af220b24b4df
Umarmajeed711 <umarmajeed711@gmail.com>
2025-01-26 10:28:13 +0500

    furniture

---
+++ src/app/components/Home.tsx
@@ -0,0 +1,286 @@
+// import Link from "next/link"
+// import "./home.css"
+import '../Style.css'
+// import Image from "next/image";
+import { Image } from "react-bootstrap"
+// import { text } from "stream/consumers";
+import Carousel from 'react-bootstrap/Carousel';
+
+
+export const Home = () => {
+  return (
+    <div className="main">
+
+
+        {/* Hero Section */}
+        <div className=" heroSection  row">
+
+          <div className="d-flex flex-wrap heroTitle col-4" >
+             <p className="h1">Rocket single seater</p>
+             <p className="underButton">Shop Now</p>
+          </div>
+
+          {/* Hero image */}
+          <div  className="col-7">
+            {/* <Image src={"/SingleSeater.png"}  alt="" width={800} height={800}/> */}
+
+            <Image src="/SingleSeater.png" alt="" className="heroImage"  />
+          </div>
+        </div>
+
+
+
+
+         {/* main menu */}
+        <div className="d-flex p-5  sideTableDiv row" >
+
+
+          {/* side div 1 */}
+          <div className="d-flex flex-column sideDiv col-sm-6 col-10">
+            <Image  src="/SideTable/Mask group.png" alt="mask group"  style={{height:"100%"}} />
+
+            <div className="side">
+            <p className='sideTable'>Side Table</p>
+            <span className="underButton">View More</span>
+            </div>
+          </div>
+
+           {/* side div 2 */}
+          <div className="d-flex flex-column sideDiv col-sm-6 col-10">
+             <Image  src="/SideTable/cloudSofa.png" alt="cloud sofa" style={{height:"100%"}}/>
+            <div className="side">
+            <p className='sideTable'>Side Table</p>
+            <span className="underButton">View More</span>
+            </div> 
+          </div>
+
+
+        </div>
+
+
+        {/* Top Picks for You */}
+
+       <div className="p-3 pt-5" >
+        <div style={{textAlign:"center"}}>
+          <h3> Top Picks FOr You</h3>
+          <p className="toppicspara">Find a bright ideal to suit your taste with our great selection of suspension, floor and table lights.</p>
+        </div>
+
+          {/* Top pics posts */}
+          <div className="container">
+
+          
+        <div className="mt-5 row ">
+          <div className="mycards p-2 col-lg-3 col-md-6 ">
+            <div style={{height:"287px",width:"100%"}}>
+            <Image src="/ToppicsForyou/TrentorSofa.png" alt="" />
+            </div>
+            <p className="title">Trentor Modular sofa-3</p>
+            <p className="price">Rs. 25,000</p>
+          </div>
+
+          <div className="mycards p-2 col-lg-3 col-md-6 ">
+          <div style={{height:"287px",width:"100%"}}>
+            <Image src="/ToppicsForyou/Granite dining table with dining chair 1.png" alt=""  />
+            </div>
+            <p className="title">Granite dining table with dining chair</p>
+            <p className="price">Rs. 25,000</p>
+          </div>
+
+
+          <div className="mycards p-2 col-lg-3 col-md-6 ">
+          <div style={{height:"287px",width:"100%"}}>
+            <Image src="/ToppicsForyou/Mask group (1).png" alt="" />
+          </div>
+          <p className="title">Outdoor bar table and stool</p>
+          <p className="price">Rs. 25,000</p>
+          </div>
+
+
+          <div className="mycards p-2 col-lg-3 col-md-6 ">
+          <div style={{height:"287px",width:"100%",textAlign:"center"}}>
+            <Image src="/ToppicsForyou/Mask group (2).png" alt="" />
+          </div>
+          <p className="title">Plain console with teak mirror</p>
+          <p className="price">Rs. 25,000</p>
+          </div>
+
+        </div>
+
+         <div className="mt-5" style={{textAlign:'center'}}>
+         <span className="underButton">View More</span>
+         </div>
+
+         </div>
+
+        </div>
+
+
+
+        {/* Asgard Sofa */}
+
+        <div className="d-flex align-items-center px-2 asgardSofa row">
+
+          <div className="col-sm-8 col-12 asgardHeight">
+            <Image src="/Asgaard sofa 1.png" alt="" style={{width:"100%",height:"100%"}} />
+          </div>
+
+          <div className="p-5 newArrivals col-sm-4 col-12" >
+             <p className="newArrivalHeading">New Arrivals</p>
+             <p className="asgardHeading" >Asgaard Sofa</p>
+             <br />
+             <span className=" orderButton">Order Now</span>
+          </div>
+        </div>
+
+
+
+        {/* Our Blogs */}
+        
+        <div className="ourBlogs pt-5 p-3 mt-3">
+        <div style={{textAlign:"center"}}>
+          <h3>Our Blogs</h3>
+          <p className="toppicspara" >Find a bright ideal to suit your taste with our great selection of suspension, floor and table lights.</p>
+        </div>
+
+
+         {/* Top pics div */}
+        <div  className="mt-5 topPics">
+
+        {/* // Post */}
+        <div className="container   article" >
+
+
+         <div className="row">
+
+          {/* post 1 div */}
+          <div style={{textAlign:"center"}} className="col-12 col-sm-4" >
+            <div className="blogImage">
+            <Image src="/OurBlogs/Rectangle 13.png" alt="rectangle.13" style={{height:"100%",width:"100%"}}  />
+            </div>
+            
+
+
+            <div className="p-3 mt-3 blogData">
+               <p>Going all-in with millenail design</p>
+               <span className="underButton">Read More</span>
+
+               <div className="d-flex gap-2 justify-content-center mt-3">
+                <p>0</p>
+                <p>12:34 892-92</p>
+               </div>
+            </div>
+
+            
+    
+          </div>
+
+           {/* post 2 div */}
+          <div  style={{textAlign:"center"}} className="col-12 col-sm-4">
+            <div className="blogImage">
+            <Image src="/OurBlogs/Rectangle 14.png" alt="Rectangle.14"  style={{height:"100%",width:"100%"}} />
+            </div>
+          
+
+           <div className="p-3 mt-3 blogData" >
+           <p>Going all-in with millenail design</p>
+           <span className="underButton">Read More</span>
+
+               <div className="d-flex gap-2 justify-content-center mt-3">
+                <p>0</p>
+                <p>12:34 892-92</p>
+               </div>
+            </div>
+
+
+          </div>
+
+           {/* post 3 div */}
+          <div  style={{textAlign:"center"}} className="col-12 col-sm-4">
+            <div className="blogImage">
+            <Image src="/OurBlogs/Rectangle 15.png" alt="Rectangle.15"  style={{height:"100%",width:"100%"}} />
+            </div>
+           
+
+            <div className="p-3 mt-3 blogData">
+            <p>Going all-in with millenail design</p>
+            <span className="underButton">Read More</span>
+
+               <div className="d-flex gap-2 justify-content-center mt-3">
+                <p>0</p>
+                <p>12:34 892-92</p>
+               </div>
+            </div>
+
+    
+          </div>
+
+          </div>
+
+        </div>
+
+
+         {/* Its a crousel which only shows in the mobile screens */}
+        <Carousel className="hideOnPC">
+      {/* carousal 1 */}
+      <Carousel.Item interval={1000} className='crouselItem'>
+      <Image src="/OurBlogs/Rectangle 13.png" alt=""  style={{width:"100vw", height:"100%"}}/>
+        <Carousel.Caption>
+        <p className="crousalText">Going all-in with millenail design</p>
+        <span className="underButton">Read More</span>
+        </Carousel.Caption>
+      </Carousel.Item>
+
+       {/* carousal 2 */}
+      <Carousel.Item interval={500} className='crouselItem'>
+      <Image src="/OurBlogs/Rectangle 14.png" alt=""  style={{width:"100vw",height:"100%" }}/>
+        <Carousel.Caption>
+        <p className="crousalText">Going all-in with millenail design</p>
+        <span className="underButton">Read More</span>
+        </Carousel.Caption>
+      </Carousel.Item>
+
+
+
+       {/* carousal 3 */}
+      <Carousel.Item className='crouselItem'>
+      <Image src="/OurBlogs/Rectangle 15.png" alt=""  style={{width:"100vw",height:"100%"}}/>
+        <Carousel.Caption>
+        <p className="crousalText">Going all-in with millenail design</p>
+        <span className="underButton">Read More</span>
+        </Carousel.Caption>
+      </Carousel.Item>
+
+    </Carousel>
+
+        {/* // View all post button */}
+
+        <div className="mt-5" style={{textAlign:'center'}}>
+         <span className="underButton">View All post</span>
+         </div>
+
+         </div>
+
+
+
+        </div>
+       
+
+
+       {/* Our Instagram  */}
+
+       <div>
+        <div className="d-flex justify-content-center align-items-center  ourInstagram">
+
+          <div className="d-flex justify-content-center align-items-center flex-column" >
+          <p className="instaHeading">Our Instagram</p>
+          <p className="instaFollow">Follow our store on Instagram</p> 
+          <p  className="d-flex justify-content-center align-items-center followUs" >Follow Us</p>
+          </div>
+
+        </div>
+       </div>
+        
+    </div>
+  ) 
+}
---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,91 @@
+'use client'
+import Link from "next/link"
+import "./navbar.css"
+import { FaRegUser } from "react-icons/fa6";
+
+import { FiSearch } from "react-icons/fi";
+import { FaRegHeart } from "react-icons/fa";
+// import { FiAlignJustify } from "react-icons/fi";
+// import { RxHamburgerMenu } from "react-icons/rx";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { IoMdClose } from "react-icons/io";
+import { useState } from "react";
+import { GrCart } from "react-icons/gr";
+
+
+export const Navbar = () => {
+
+
+ const [showSideBar, setShowSideBar] = useState(false);
+
+ const showBar = () => {
+    setShowSideBar(true)
+ }
+ 
+ const hideBar = () => {
+    setShowSideBar(false)
+ }
+
+  return (
+    <header>
+        {/* navbar */}
+        <nav className="nav" >
+            {/* logo */}
+            <div>
+                LOGO
+            </div>
+
+            {/* links & icons */}
+            <div className="d-flex align-items-center gap-5 ">
+
+            
+            <ul className="nav-links gap-5">
+                <li><Link href="/" className="link active">Home</Link></li>
+                <li><Link href="/" className="link">Shop</Link></li>
+                <li><Link href="/" className="link">About</Link></li>
+                <li><Link href="/" className="link">Contact</Link></li>
+                
+            </ul>
+
+            <div className="nav-icons gap-4 ">
+                
+                 <p><Link href="/" className="link"><FaRegUser /></Link></p> 
+                <p><FiSearch/></p>
+                <p><Link href="/" className="link"><FaRegHeart /></Link></p>
+                <p><Link href="/" className="link"><GrCart/></Link></p>  
+                
+
+                
+                {/* <p><FaRegUser /></p>
+                <p><FiSearch/></p>
+                <p><FaRegHeart /></p>
+                <p><GrCart/></p> */}
+            </div>
+
+            
+            </div>
+
+             {/* button for side bar */}
+            <p onClick={showBar} className="hideOnPc">< GiHamburgerMenu/></p>
+           
+        </nav>
+
+        {/* navbar links for mobile */}
+        {
+            (showSideBar) ? 
+
+        <div className="sideBar">
+         <p onClick={hideBar} className="closeIcon"><IoMdClose/></p>
+        <ul className="sideBarLinks">
+                <li><Link href="/" className="link ">Home</Link></li>
+                <li><Link href="/" className="link">Shop</Link></li>
+                <li><Link href="/" className="link">About</Link></li>
+                <li><Link href="/" className="link">Contact</Link></li>
+        </ul> 
+        </div>
+        : null
+}
+
+    </header>
+  )
+}
